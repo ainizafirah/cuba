8411cf75fcfbf4fd0684f8967e124b6aef9ecc75
ainizafirah <45682356+ainizafirah@users.noreply.github.com>
2018-12-10 10:27:19 +0800

    add variables

--- app/Test.php
+++ app/Test.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Test extends Model
 {
-    //
+    
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', 'age', 'phone number' , 'address'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    
 }
