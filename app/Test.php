b79a3da077b13aa6c09bad2f77196270ce6200c6
ainizafirah <45682356+ainizafirah@users.noreply.github.com>
2018-12-10 10:36:35 +0800

    deleted variables

--- app/Test.php
+++ app/Test.php
@@ -6,25 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Test extends Model
 {
-    
-    use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password', 'age', 'phone number' , 'address'
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-    
+    //
 }
